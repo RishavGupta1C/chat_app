411300d1509c614c37d952340c0d73f28a877cc0
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-11-22 17:55:05 +0530

    forth commit validating user input

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -10,7 +10,24 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+
   var _isLogin = true;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    // trigger validators here and save the email and password input
+    // currentState won't be null as _submit will only get triggered after
+    // form is created, So _form will be available
+    final isValid = _form.currentState!.validate();
+
+    if (isValid) {
+      _form.currentState!.save();
+      print(_enteredEmail);
+      print(_enteredPassword);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,62 +53,87 @@ class _AuthScreenState extends State<AuthScreen> {
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: SingleChildScrollView(
-                    // child: Form(
-                    child: Column(
-                      // This column will take as much space as needed by the content
-                      // mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Email Address',
+                    child: Form(
+                      // Form is mainly used for _formKey
+                      key: _form,
+                      child: Column(
+                        // This column will take as much space as needed by the content
+                        // mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Email Address',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
+                                return 'Please enter a valid email address.';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              // as the save (inside _submit) is called only
+                              // after validation we can use '!'
+                              _enteredEmail = value!;
+                            },
                           ),
-                          keyboardType: TextInputType.emailAddress,
-                          autocorrect: false,
-                          textCapitalization: TextCapitalization.none,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Password',
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                            ),
+                            obscureText: true,
+                            obscuringCharacter: '*',
+                            validator: (value) {
+                              if (value == null || value.trim().length < 6) {
+                                return 'Password must be atleast 6 characters long.';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _enteredPassword = value!;
+                            },
                           ),
-                          obscureText: true,
-                          obscuringCharacter: '*',
-                        ),
-                        const SizedBox(height: 10),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            // Background
-                            primary:
-                                Theme.of(context).colorScheme.primaryContainer,
-                            // Foreground
-                            onPrimary: Theme.of(context).colorScheme.primary,
-                            // BorderRadius
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
+                          const SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: _submit,
+                            style: ElevatedButton.styleFrom(
+                              // Background
+                              primary: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                              // Foreground
+                              onPrimary: Theme.of(context).colorScheme.primary,
+                              // BorderRadius
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            child: Text(
+                              _isLogin ? 'Login' : 'Sign-up',
                             ),
                           ),
-                          child: Text(
-                            _isLogin ? 'Login' : 'Sign-up',
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              _isLogin = !_isLogin;
-                            });
-                          },
-                          child: Text(
-                            _isLogin
-                                ? 'Create an account'
-                                : 'I already have an account',
-                          ),
-                        )
-                      ],
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(
+                              _isLogin
+                                  ? 'Create an account'
+                                  : 'I already have an account',
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-              // ),
             ],
           ),
         ),
